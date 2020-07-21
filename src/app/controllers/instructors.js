2a8d1b83abee37038593f7a49efbb7db53a20947
DavdVilaronga <davidvilaronga@hotmail.com>
2020-07-21 16:20:48 -0300

    db-working-2

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,8 +1,12 @@
 const {age, date} = require('../../lib/utils')
+const Instructor = require('../models/Instructor')
 
 module.exports = {
     index(req, res) {
-        return res.render("instructors/index")
+        Instructor.all(function(instructors) {
+            return res.render("instructors/index", {instructors})
+        })
+
     },
     create(req, res) {
         return res.render('instructors/create')
@@ -15,15 +19,31 @@ module.exports = {
             }
         }
 
-        let {avatar_url, birth, name, services, gender} = req.body
+        Instructor.create(req.body, function(instructor){
+            return res.redirect(`/instructors/${instructor.id}`)
+        })
 
-        return
+
     },
     show(req, res) {
-        return
+        Instructor.find(req.params.id, function(instructor){
+            if(!instructor) return res.send("Instructor not Found!")
+
+            instructor.age = age(instructor.birth)
+            instructor.services = instructor.services.split(",")
+            instructor.created_at = date(instructor.created_at).format
+
+            return res.render("instructors/show", { instructor })
+        })
     },
     edit(req, res) {
-        return
+        Instructor.find(req.params.id, function(instructor){
+            if(!instructor) return res.send("Instructor not Found!")
+
+            instructor.birth = date(instructor.birth).iso
+
+            return res.render("instructors/edit", { instructor })
+        })
     },
     put(req, res) {
         const keys = Object.keys(req.body)
@@ -32,9 +52,14 @@ module.exports = {
                 return res.send('Please, fill all fields!')
             }
         }
-        return
+        
+        Instructor.update(req.body, function() {
+            return res.redirect(`/instructors/${req.body.id}`)
+        })
     },
     delete(req, res) {
-        return
+        Instructor.delete(req.body.id, function() {
+            return res.redirect(`/instructors`)
+        })
     },
 }
